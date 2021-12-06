d3a98ec02468b278965e04b12c0899770c0016a5
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2021-12-06 00:34:17 -0600

    commit tree 5e64dfa107e35b1f9bc0f2ef4d9030de2b74be96

--- src/App.js
+++ src/App.js
@@ -2,45 +2,62 @@ import React from 'react';
 import './App.css';
 import { CATEGORIES } from './data'
 
+const tasks = [
+  {
+    text: 'Buy rice',
+    category: 'Food'
+  },
+  {
+    text: 'Save a tenner',
+    category: 'Money'
+  },
+  {
+    text: 'Build a todo app',
+    category: 'Code'
+  },
+  {
+    text: 'Build todo API',
+    category: 'Code'
+  },
+  {
+    text: 'Get an ISA',
+    category: 'Money'
+  },
+  {
+    text: 'Cook rice',
+    category: 'Food'
+  },
+  {
+    text: 'Tidy house',
+    category: 'Misc'
+  }
+]
+
 class App extends React.Component {
 
   state = {
-    tasks: [
-      {
-        text: 'Buy rice',
-        category: 'Food'
-      },
-      {
-        text: 'Save a tenner',
-        category: 'Money'
-      },
-      {
-        text: 'Build a todo app',
-        category: 'Code'
-      },
-      {
-        text: 'Build todo API',
-        category: 'Code'
-      },
-      {
-        text: 'Get an ISA',
-        category: 'Money'
-      },
-      {
-        text: 'Cook rice',
-        category: 'Food'
-      },
-      {
-        text: 'Tidy house',
-        category: 'Misc'
-      }
-    ]
+    ...tasks
   }
 
+
+
   render() {
+    const categButtons = CATEGORIES.map((c, indx) => {
+      return <button key={indx}>{c}</button>
+    })
     return (
       <div className="App">
         <h2>My tasks</h2>
+        <h5>Categories Filters</h5>
+      <div className='categories'>
+        {categButtons}
+      </div>
+      <div className='tasks'>
+      <h5>Tasks</h5>
+      </div>
+      
+       
+        
       </div>
     );
   }
